1a7e51cd44028051c7ddee21e3d5e64f28412455
berezzin <nexchanel@gmail.com>
2022-04-18 17:21:07 +0300

    https://docs.djangoproject.com/en/4.0/intro/tutorial05/#improving-our-view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 
@@ -36,3 +37,8 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def get_queryset(self):
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
